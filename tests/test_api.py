4b80394594857e18406537308a8ee48a5d099ae1
Sam Scott <sam.scott7@me.com>
2022-05-27 14:58:24 +0100

    Tests for HTTP API DataProvider

--- tests/test_api.py
+++ tests/test_api.py
@@ -4,13 +4,40 @@ from http import HTTPStatus
 
 import pytest
 
-from nb_open_prescribing.api import OpenPrescribingHttpApi
+from nb_open_prescribing.api import HttpApiDataProvider, OpenPrescribingHttpApi
 from nb_open_prescribing.model import (
     CCGBoundaries,
     CCGSpend,
     Feature,
     FeatureCollection,
 )
+
+SPENDING_BY_CCG_TEST_JSON_DATA = [
+    CCGSpend(
+        items=600,
+        quantity=10000.0,
+        actual_cost=12345.67,
+        date=date(2022, 1, 1),
+        row_id="ABC",
+        row_name="ANOTHER CCG",
+    ),
+    CCGSpend(
+        items=700,
+        quantity=20250.0,
+        actual_cost=23456.78,
+        date=date(2022, 2, 1),
+        row_id="ABC",
+        row_name="ANOTHER CCG",
+    ),
+    CCGSpend(
+        items=800,
+        quantity=30500.0,
+        actual_cost=34567.89,
+        date=date(2022, 3, 1),
+        row_id="ABC",
+        row_name="ANOTHER CCG",
+    ),
+]
 
 
 @pytest.fixture
@@ -26,32 +53,9 @@ def mock_query_spending_by_ccg_api_response(mocker, spend_json):
 
 def test_query_spending_by_ccg(mock_query_spending_by_ccg_api_response):
     api = OpenPrescribingHttpApi()
-    assert api.query_spending_by_ccg() == [
-        CCGSpend(
-            items=600,
-            quantity=10000.0,
-            actual_cost=12345.67,
-            date=date(2022, 1, 1),
-            row_id="ABC",
-            row_name="ANOTHER CCG",
-        ),
-        CCGSpend(
-            items=700,
-            quantity=20250.0,
-            actual_cost=23456.78,
-            date=date(2022, 2, 1),
-            row_id="ABC",
-            row_name="ANOTHER CCG",
-        ),
-        CCGSpend(
-            items=800,
-            quantity=30500.0,
-            actual_cost=34567.89,
-            date=date(2022, 3, 1),
-            row_id="ABC",
-            row_name="ANOTHER CCG",
-        ),
-    ]
+    response = api.query_spending_by_ccg()
+    mock_query_spending_by_ccg_api_response.assert_called_once()
+    assert response == SPENDING_BY_CCG_TEST_JSON_DATA
 
 
 @pytest.fixture
@@ -103,6 +107,7 @@ def test_query_org_location(mock_query_org_location_api_response):
     api = OpenPrescribingHttpApi()
     actual = api.query_org_location()
     expected = CCGBoundaries(FEATURE_COLLECTION_TEST_JSON_DATA)
+    mock_query_org_location_api_response.assert_called_once()
     assert actual.crs == expected.crs
     assert actual.features == expected.features
 
@@ -143,4 +148,23 @@ def test__search_handles_api_params_json_format(mock_requests_response):
     mock_requests_response.assert_called_once_with(
         "https://openprescribing.net/api/1.0/org_location",
         params=ChainMap({"format": "json"}, {"format": "csv", "still": "json"}),
+    )
+
+
+def test_http_api_data_provider_get_chemical_spending_for_ccg(
+    mock_query_spending_by_ccg_api_response,
+):
+    provider = HttpApiDataProvider()
+    response = provider.chemical_spending_for_ccg(chemical="BADF00D", ccg="ABC")
+    mock_query_spending_by_ccg_api_response.assert_called_once_with(
+        path="spending_by_ccg", api_params={"code": "BADF00D", "org": "ABC"}
+    )
+    assert response == SPENDING_BY_CCG_TEST_JSON_DATA
+
+
+def test_http_api_data_provider_gets_ccg_boundaries(mock_query_org_location_api_response):
+    provider = HttpApiDataProvider()
+    provider.ccg_boundaries()
+    mock_query_org_location_api_response.assert_called_once_with(
+        path="org_location", api_params={"org_type": "ccg"}
     )
